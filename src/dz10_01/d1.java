a8518a02b9aadd6095b1720c8486a525a3db0e43
Stefan Jocic <jocicstefan91@gmail.com>
2022-01-10 22:44:40 +0100

    domaci1

---
+++ src/dz10_01/d1.java
@@ -0,0 +1,22 @@
+package dz10_01;
+
+public class d1 {
+
+	public static void main(String[] args) {
+		
+		Proizvod p1 = new Proizvod();
+		
+		p1.cena=230.15;
+		p1.proizvod="Milka";
+		p1.tezina=200;
+		p1.print();
+		
+		Proizvod p2 = new Proizvod();
+		
+		p2.cena=45;
+		p2.proizvod="hleb";
+		p2.tezina=500;
+		p2.print();
+	}
+
+}
